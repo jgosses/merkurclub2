67bd82e98239a77c8f4d8489ccf861e38c53b120
Jimmy Gosses <gosses.jim@gmail.com>
2020-01-16 20:01:47 -0500

    additional items for test secondary slider

--- js/main.js
+++ js/main.js
@@ -11,3 +11,39 @@ const mySiema = new Siema({
 });
 document.querySelector('.prev').addEventListener('click', () => mySiema.prev());
 document.querySelector('.next').addEventListener('click', () => mySiema.next());
+
+
+var slideIndex = 1;
+var slideIndex2 = 2;
+showDivs (slideIndex);
+showDivs2 (slideIndex2);
+
+function plusDivs(n) {
+    showDivs (slideIndex += n);
+    showDivs2 (slideIndex2 += n);
+}
+
+function showDivs(n) {
+    var i;
+    var x = document.getElementsByClassName("mySlides");
+    
+    if (n > x.length) {slideIndex = 1}
+    if (n < 1) {slideIndex = x.length} ;
+    for (i = 0; i < x.length; i++) {
+        x[i].style.display = "none";
+    }
+    
+    x[slideIndex-1].style.display = "block";
+}
+function showDivs2(n) {
+    var i;
+    var y = document.getElementsByClassName("mySlides2");
+    
+    if (n > y.length) {slideIndex2 = 1}
+    if (n <1) {slideIndex2 = y.length} ;
+    for (i = 0; i < y.length; i++) {
+        y[i].style.display = "none";
+    }
+
+    y[slideIndex2-1].style.display = "block";
+}
